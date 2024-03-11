712b5efe048559938cc5fd255599a8b5fe6af762
Amelia Johnson <111928731+amelia-online@users.noreply.github.com>
2024-03-11 11:43:25 -0600

    trying to add string support

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,76 @@ fn main() {
         }
         match input.trim() {
             "quit" => break,
-            _ => println!("<< {}", input.to_string()),
+            _ => {}
         }
     }
 }
 
+fn roll(dice: DiceType) -> u32 {
+    use DiceType::*;
+    let mut rng = rand::thread_rng();
+    let lower = 1;
+    let mut upper = 20;
+    match dice {
+        D4 => upper = 4,
+        D6 => upper = 6,
+        D8 => upper = 8,
+        D10 => upper = 10,
+        D12 => upper = 12,
+        D20 => {}
+    }
+    rng.gen_range(lower..upper)
+}
+
+fn extract_string(index: &mut usize, chars: &Vec<char>) -> Option<String> {
+    let mut result = String::new();
+
+    if *index == chars.len() - 1 {
+        return None;
+    }
+
+    // index starts on a "
+    // and it'll end on a "
+
+    *index += 1;
+    let mut chr = chars[*index];
+
+    while chr != '\"' {
+        if *index == chars.len() - 1 {
+            return None;
+        }
+
+        chr = chars[*index];
+
+        match chr {
+            '\\' => {
+                *index += 1;
+                if let ch = chars[*index] {
+                    match ch {
+                        'n' => result.push('\n'),
+                        't' => result.push('\t'),
+                        'r' => result.push('\r'),
+                        '\\' => result.push('\\'),
+                        '\'' => result.push('\''),
+                        '\"' => result.push('\"'),
+                        _ => return None,
+                    }
+                } else {
+                    return None;
+                }
+            }
+            _ => result.push(chr),
+        }
+
+        *index += 1;
+    }
+
+    Some(result)
+}
+
 fn lex(string: &String) -> Vec<Token> {
     let mut result: Vec<Token> = Vec::new();
-    let mut line_num = 1;
+    let mut line_num = 0;
     let mut index = 0;
 
     fn report_error(line: u32, msg: &str) {
@@ -35,26 +97,33 @@ fn lex(string: &String) -> Vec<Token> {
             '\n' => {
                 line_num += 1;
                 index += 1;
-                continue;
             }
             ' ' => {
                 index += 1;
-                if let Some(token) = process_string(&buf) {
-                    result.push(token);
+            }
+            '\"' => {
+                if let Some(literal) = extract_string(&mut index, &chars) {
+                    result.push(Token::Literal(literal));
+                    index += 1;
+                } else {
+                    report_error(line_num, "Invalid string encountered.");
                 }
-                buf.clear();
             }
             _ => {
                 buf.push(chr);
                 index += 1;
+                continue;
             }
         }
-    }
 
-    if let Some(token) = process_string(&buf) {
-        result.push(token);
+        if let Some(token) = process_string(&buf) {
+            result.push(token);
+        } else {
+            report_error(line_num, "Unknown token encountered.");
+        }
+
+        buf.clear();
     }
-    buf.clear();
 
     result
 }
@@ -62,6 +131,15 @@ fn lex(string: &String) -> Vec<Token> {
 fn process_string(string: &String) -> Option<Token> {
     use DiceType::*;
     use Token::*;
+
+    if string.is_empty() {
+        return None;
+    }
+
+    if let Ok(num) = string.parse::<f64>() {
+        return Some(Number(num));
+    }
+
     match string.trim() {
         "d4" | "D4" => Some(Dice(D4)),
         "d6" | "D6" => Some(Dice(D6)),
@@ -70,8 +148,17 @@ fn process_string(string: &String) -> Option<Token> {
         "d12" | "D12" => Some(Dice(D12)),
         "d20" | "D20" => Some(Dice(D20)),
         "roll" => Some(Roll),
+        "create" => Some(Create),
+        "*" => Some(Mult),
+        "/" => Some(Divide),
+        "+" => Some(Add),
+        "-" => Some(Minus),
+        "^" => Some(Pow),
+        "%" => Some(Mod),
+        "->" => Some(LittleArrow),
+        "=>" => Some(BigArrow),
 
-        _ => None,
+        _ => Some(Ident(string.to_owned())),
     }
 }
 
@@ -97,11 +184,16 @@ enum DiceType {
 enum Token {
     Dice(DiceType),
     Roll,
-    Mult(f64, f64),
-    Divide(f64, f64),
-    Add(f64, f64),
-    Minus(f64, f64),
-    Pow(f64, f64),
-    Mod(f64, f64),
+    Mult,
+    Divide,
+    Add,
+    Minus,
+    Pow,
+    Mod,
     Ident(String),
+    Number(f64),
+    Create,
+    LittleArrow,
+    BigArrow,
+    Literal(String),
 }
